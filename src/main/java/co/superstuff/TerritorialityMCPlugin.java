0fb3a6a4f33c3e5ff6e83275d4de3f0b50966ca2
David256 <wasterdavid@gmail.com>
2023-09-28 16:44:37 -0500

    change: add prototype for the command register response

--- src/main/java/co/superstuff/TerritorialityMCPlugin.java
+++ src/main/java/co/superstuff/TerritorialityMCPlugin.java
@@ -1,4 +1,9 @@
 package co.superstuff;
+import org.bukkit.command.BlockCommandSender;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TerritorialityMCPlugin extends JavaPlugin {
@@ -11,5 +16,19 @@ public class TerritorialityMCPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         System.out.println("Territoriality is off");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("register")) {
+            if (sender instanceof Player player) {
+                player.sendMessage("Register...");
+            } else if (sender instanceof ConsoleCommandSender) {
+                sender.sendMessage("Only user can call this command");
+            } else if (sender instanceof BlockCommandSender) {
+                System.out.println("The command /register was run by a command block");
+            }
+        }
+        return true;
     }
 }
